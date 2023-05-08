9bd0978c9e2f198ff37ef29c8046236d6c1fab2b
Akumaxblend <83503168+Akumaxblend@users.noreply.github.com>
2023-05-08 18:27:36 +0200

    initial setup

---
+++ filmsweb.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+from flask import Flask,request,render_template,jsonify,abort, redirect
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+CORS(app)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="FilmsWeb",
+    password="password",
+    database="FilmsWeb"
+)
+
+mycursor = mydb.cursor()
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+# Création des différentes tables 
+
+# mycursor.execute('''drop table if exists Genre CASCADE CONSTRAINTS PURGE''')
+# mycursor.execute('''drop table if exists Nationalite CASCADE''')
+# mycursor.execute('''drop table if exists Utilisateur CASCADE''')
+# mycursor.execute('''drop table if exists AVu CASCADE''')
+# mycursor.execute('''drop table if exists AAime CASCADE''')
+# mycursor.execute('''drop table if exists Commentaire CASCADE''')
+# mycursor.execute('''drop table if exists Film CASCADE''')
+
+
+
+#mycursor.execute('''create table Genre (idGenre int primary key auto_increment, nomG varchar(50))''')
+
+#mycursor.execute('''create table Nationalite (idNationalite int primary key auto_increment, nomN varchar(50))''')
+
+#mycursor.execute('''create table Film (idFilm int primary key auto_increment,  titre varchar(50), duree int, anneSortie int, synopsis varchar(140), idGenre int, idNationalite int, CONSTRAINT fk_idgenre FOREIGN KEY (idGenre) REFERENCES Genre(idGenre), CONSTRAINT fk_idnationalite FOREIGN KEY (idNationalite) REFERENCES Nationalite(idNationalite))''')
+
+#mycursor.execute(''' create table Utilisateur (idUser int primary key auto_increment, nom varchar(50), prenom varchar(50), pseudo varchar(50), mail varchar(50), mdp varchar (50))''')
+
+#mycursor.execute('''create table AVu(idVu int primary key auto_increment, idFilm int, idUser int, CONSTRAINT fk_idFilm FOREIGN KEY (idFilm) REFERENCES Film(idFilm), CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser))''')
+
+#mycursor.execute('''create table AAime(idAime int primary key auto_increment, idFilm int, idUser int, CONSTRAINT fk_idFilm2 FOREIGN KEY (idFilm) REFERENCES Film(idFilm), CONSTRAINT fk_idUser2 FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser))''')
+
+#mycursor.execute('''create table Commentaire(idCom int primary key auto_increment, texte varchar(140), idFilm int, idUser int, CONSTRAINT fk_idFilm3 FOREIGN KEY (idFilm) REFERENCES Film(idFilm), CONSTRAINT fk_idUser3 FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser))''')
+
+#mydb.commit()
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+
+# mycursor.execute('''insert into etudiants values(1,'bob leponge'), (2, 'dora lexploratrice'), (3, 'koro-sensei')''')
+# mydb.commit()
+
+
+# ajout = input("quel nom voulez vous ajouter ?")
+
+# mycursor.execute('''Insert into etudiants values(4,%s)''',(ajout,))
+# mydb.commit()
+
+# mycursor.execute('''Delete from etudiants where id = 2''')
+# mydb.commit()
+
+# mycursor.execute('''select * from etudiants''')
+# etuds = mycursor.fetchall()
+# print(etuds)
+
+@app.route("/")
+def index():
+    
+    #return ("<html><body><p1>Hello world</p1></body></html>")
+    return render_template("liste.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
