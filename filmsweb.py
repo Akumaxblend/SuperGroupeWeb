584715a68d65327b6f760786d98d18bceb1c24d7
SégolèneLazarus <seg.laz@ik.me>
2023-05-29 18:30:15 +0200

    created_at

--- filmsweb.py
+++ filmsweb.py
@@ -134,7 +134,7 @@ def show_film(idFilm):
 
     for tmp in mycursor:
 
-        commentaires.append({"texte":tmp[1], "pseudo":tmp[2]})
+        commentaires.append({"created_at":tmp[0],"texte":tmp[1], "pseudo":tmp[2]})
 
     return render_template("/film.html", c = commentaires, film=film)
 
